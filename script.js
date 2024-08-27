06ad1d5a97fee215b4b3add680a2d0d30d8591f9
luciendberkeley <lucienduchateau@students.berkeley.net>
2024-08-27 18:41:19 +0000

    teasdkjhjs

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const ws = new WebSocket('ws://localhost:8080/');
+const ws = new WebSocket('wss://ideal-computing-machine-4jg5pvrj9wjw27764-8080.app.github.dev/');
 
 ws.onopen = (evnet) => {
   ws.send('something');
 };
 
 ws.onmessage = (event) => {
-  console.log('received: %s', data);
+  console.log('received: %s', event.data);
 };
+
+document.addEventListener("keypress", (event) => {
+  if(event.key == "enter") {
+    let input_box = document.querySelector(".message_box");
+    let value = input_box.value;
+
+    input_box.value = 0;
+    ws.send(value)
+  }
+})
