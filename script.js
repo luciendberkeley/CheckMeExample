7e81fbdd1c0531ae40e55785baea640296b55844
luciendberkeley <lucienduchateau@students.berkeley.net>
2024-08-28 19:00:51 +0000

    Fixed WS, Logging stuff

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const ws = new WebSocket('wss://ideal-computing-machine-4jg5pvrj9wjw27764-8080.app.github.dev/');
+const ws = new WebSocket('wss://ideal-computing-machine-4jg5pvrj9wjw27764-5050.app.github.dev/');
+
+const log = (message) => {
+  const log_e = document.querySelector('.log');
+  log_e.innerText += `\n${message}`;
+}
 
 ws.onopen = (evnet) => {
   ws.send('something');
@@ -6,14 +11,17 @@ ws.onopen = (evnet) => {
 
 ws.onmessage = (event) => {
   console.log('received: %s', event.data);
+
+  log(`Recived: ${event.data}`);
 };
 
-document.addEventListener("keypress", (event) => {
-  if(event.key == "enter") {
+document.addEventListener("keydown", (event) => {
+  if(event.key == "Enter") {
     let input_box = document.querySelector(".message_box");
     let value = input_box.value;
 
-    input_box.value = 0;
-    ws.send(value)
+    input_box.value = "";
+    ws.send(value);
+    log(`Sent: ${value}`);
   }
 })
--- wss.js
+++ wss.js
@@ -1,6 +1,6 @@
 let ws = require("ws");
 
-const wss = new ws.WebSocketServer({ port: 8080 });
+const wss = new ws.WebSocketServer({ port: 5050 });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
